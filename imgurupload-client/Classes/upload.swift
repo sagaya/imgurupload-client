180611a3c4f9f126550ab49ff6b51765ecd7de07
cosmo1234 <harpheezsagaya@gmail.com>
2017-04-01 18:16:30 +0100

    Changed README

--- imgurupload-client/Classes/upload.swift
+++ imgurupload-client/Classes/upload.swift
@@ -10,11 +10,10 @@ import UIKit
 import Alamofire
 
 open class ImgurUpload {
-    open static var baseUrl = "https://api.imgur.com/3/upload"
-    open static var imageData:Data?
-    open static var apiKey:String?
+    internal static var baseUrl = "https://api.imgur.com/3/upload"
+    internal static var imageData:Data?
+    internal static var apiKey:String?
     
-    internal static var touchHandler: (() -> DataResponse<Any>)?
 
     open static func upload(imageData: Data = imageData!, apiKey: String = apiKey!, completionHandler: @escaping (DataResponse<Any>?) ->()){
         let headers: HTTPHeaders = ["Authorization": "Client-ID \(apiKey)"]
