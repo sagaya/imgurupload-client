b5d94c75f906d4ff5a43b747aab460072afd0e2b
cosmo1234 <harpheezsagaya@gmail.com>
2017-04-01 17:43:58 +0100

    Initial release

--- imgurupload-client/Classes/upload.swift
+++ imgurupload-client/Classes/upload.swift
@@ -6,4 +6,20 @@
 //
 //
 
-import Foundation
+import UIKit
+import Alamofire
+
+open class ImgurUpload {
+    open static var baseUrl = "https://api.imgur.com/3/upload"
+    open static var imageData:Data?
+    open static var apiKey:String?
+    
+    internal static var touchHandler: (() -> DataResponse<Any>)?
+
+    open static func upload(imageData: Data = imageData!, apiKey: String = apiKey!, completionHandler: @escaping (DataResponse<Any>?) ->()){
+        let headers: HTTPHeaders = ["Authorization": "Client-ID \(apiKey)"]
+        Alamofire.upload(imageData, to: ImgurUpload.baseUrl, method: .post, headers: headers).responseJSON { (response) in
+            completionHandler(response)
+        }
+    }
+}
